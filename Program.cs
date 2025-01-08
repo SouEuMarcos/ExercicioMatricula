3baf6bfde50fe9533c34065dab82993c7a29798c
SouEuMarcos <marcos_vinicius_015@hotmail.com>
2025-01-07 21:03:55 -0300

    Aplicando meu conhecimento classe CURSO DIO

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "Marcos";
+p1.Sobrenome = "Vinícius";
+// p1.Idade = 22;
+// p1.Apresentar();
+
+Pessoa p2 = new Pessoa();
+
+p2.Nome = "Rauana";
+p2.Sobrenome = "Lage";
+
+
+Curso cursoDeIngles = new Curso();
+
+cursoDeIngles.Nome = "Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
