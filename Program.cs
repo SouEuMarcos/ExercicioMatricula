027c12c811cdbc905507effa275e306b6476a37c
SouEuMarcos <marcos_vinicius_015@hotmail.com>
2025-01-16 19:34:34 -0300

    Introduzindo contador de alunos.

--- Program.cs
+++ Program.cs
@@ -1,16 +1,16 @@
 ﻿using ExemploExplorando.Models;
 
-Pessoa p1 = new Pessoa();
+Pessoa p1 = new Pessoa(nome: "Marcos", sobrenome: "Vinícius");
 
-p1.Nome = "Marcos";
-p1.Sobrenome = "Vinícius";
+// p1.Nome = "Marcos";
+// p1.Sobrenome = "Vinícius";
 // p1.Idade = 22;
 // p1.Apresentar();
 
-Pessoa p2 = new Pessoa();
+Pessoa p2 = new Pessoa(nome: "Rauana", sobrenome: "Lage");
 
-p2.Nome = "Rauana";
-p2.Sobrenome = "Lage";
+// p2.Nome = "Rauana";
+// p2.Sobrenome = "Lage";
 
 
 Curso cursoDeIngles = new Curso();
